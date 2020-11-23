60e7abb9f394571898fe1a002ee3133c2c315ea3
Jonathan Sieg <jonathanrsieg@gmail.com>
2020-11-22 19:14:22 -0500

    redeem working

--- tapped/src/components/Meals.js
+++ tapped/src/components/Meals.js
@@ -3,6 +3,7 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import VisibilitySensor from "react-visibility-sensor";
 import Update from "../auth/UpdateUser";
+import mealRedeem from '../assets/FreeApp.jpg';
 
 
 
@@ -10,6 +11,18 @@ import Update from "../auth/UpdateUser";
 
 
 function MealsApp(props) {
+
+
+  const [displayImage, setDisplayImage] = useState(false) 
+  
+  function imageOn() {
+    setDisplayImage(true)
+  }
+
+  function imageOff() {
+    setDisplayImage(false)
+  }
+
   const sessionToken = localStorage.getItem("sessionToken")
   const UpdateMPoints = () => {
 
@@ -53,9 +66,10 @@ function MealsApp(props) {
         <div>Meals</div>
         <br></br>
         {props.mPoints > 9 ? 
-        <button onClick={UpdateMPoints} className="RedeemButton">Redeem (1)</button>:
+        <button onClick={imageOn} className="RedeemButton">Redeem</button>:
         <button onClick={UpdateMPoints} className="CheckInButton">check in</button>
         }
+        {displayImage ? <img src={mealRedeem} alt="redeem coupon" style={{width: 200}}/> : < > </>}
       </div>
     </div>
   </div>
